00a7faf2b5fc545d996c671129da6eebeb59c26b
eider <eiderdiazmoraila@hotmail.com>
2018-05-24 11:14:02 -0700

    se actualizo la version de npm para poder instalar react-native-router-flux (checar siempre la version de npm constantemente)

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { StyleSheet,
    Text,
     View,
-     Button,
       Alert,
        Image,
    } from 'react-native';
 import ArtistBox from './ArtistBox'
 import ArtistList from './ArtistList'
 import {getArtists } from './api-client'
-//import {Scene, Router} from 'react-native-router-flux';
+import { Router, Scene } from 'react-native-router-flux';
 import HomeView from './HomeView'
 import ArtistDetailView from './ArtistDetailView'
 
@@ -17,7 +16,12 @@ import ArtistDetailView from './ArtistDetailView'
 export default class App extends React.Component {
 render() {
   // esta funcion render retorna la navegacion de las esenas home y artistdetail
-      return <HomeView></HomeView>
+      return <Router>
+          <Scene key="root">
+            <Scene key="home" component={HomeView} />
+            <Scene key="artistDetail" component={ArtistDetailView} />
+          </Scene>
+      </Router>
   }
 
 }
