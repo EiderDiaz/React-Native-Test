5a15dbd7425271a065cfd0492dca5cf450305275
eider <eiderdiazmoraila@hotmail.com>
2018-05-28 12:15:04 -0700

    todo listo firebase, justo antes de crear el signinwithcredential , (recuerda correr el comando gradlew clean dentro de la carpeta android para que todo corra bien de vez en cuando)

--- LoginView.js
+++ LoginView.js
@@ -9,13 +9,43 @@ import { StyleSheet,
 
 import FBSDK, {LoginButton,AccessToken} from 'react-native-fbsdk';
 import {Actions} from 'react-native-router-flux'
+import * as firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyDAi0k1u4rv4Dqm3DtA4xrDebdAw6WQgHs",
+  authDomain: "platzimusic-c7b96.firebaseapp.com",
+  databaseURL: "https://platzimusic-c7b96.firebaseio.com",
+  projectId: "platzimusic-c7b96",
+  storageBucket: "",
+  messagingSenderId: "285985287046"
+};
+firebase.initializeApp(config);
+
 export default class LoginView extends React.Component {
-//handleLoginFinished =
+authenticaUser(accesTokten){
+  auth.signInWithCredential(credential).then(function(user) {
+  console.log("Sign In Success", user);
+  var currentUser = user;
+  // Merge prevUser and currentUser data stored in Firebase.
+  // Note: How you handle this is specific to your application
+
+  // After data is migrated delete the duplicate user
+  return user.delete().then(function() {
+    // Link the OAuth Credential to original account
+    return prevUser.link(credential);
+  }).then(function() {
+    // Sign in with the newly linked credential
+    return auth.signInWithCredential(credential);
+  });
+}).catch(function(error) {
+  console.log("Sign In Error", error);
+});
+}
   render() {
     return (
 
       <View style={styles.container}>
-        <Text style={styles.welcome} >welcome to the jungle</Text>
+        <Text style={styles.welcome} >Welcome to the Juungle</Text>
         <LoginButton
          readPermissions={['public_profile','email']}
          onLoginFinished={
